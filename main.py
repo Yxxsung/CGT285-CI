c424b16ee99d513a3d2879459096e4d3e5ad7dd4
Sophia Alexander <sophia.alexander@aol.com>
2026-02-26 22:53:29 -0500

    Working with the code from the brightspace presentation

--- main.py
+++ main.py
@@ -3,3 +3,17 @@
 #Last Edited: 02/26/26
 
 #The goal is to take a character image, delete the background and put it on a new background
+
+#This line imports the module we are working with
+import pygame
+
+#Initializing the game
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+
+pygame.display.set_mode((800, 600))
+
+pygame.display.set_caption("Hello World")
+
+img = pygame.image.load("img/cat.jpg")
